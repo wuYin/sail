c4f4db3e62acdbd07cf35bef8cdf81ac89624539
wuYin <wuYinPost@gmail.com>
2018-11-15 19:13:05 +0800

    实现二叉树的节点计数和复制

--- binary_tree/tree.go
+++ binary_tree/tree.go
@@ -55,6 +55,7 @@ func (tree *BinaryTree) PreOrderTraverse(node *Node, f func(int)) {
 
 // 后序遍历
 // child 总在 parent 前被遍历，left 先于 right，即深度优先
+// 使用递归实现 DFS 深度优先遍历
 func (tree *BinaryTree) PostOrderTraverse(node *Node, f func(int)) {
 	if node == nil {
 		return
@@ -83,6 +84,28 @@ func (tree *BinaryTree) InOrderTraverse(node *Node, f func(int)) {
 	}
 }
 
+// BFS
+// 使用队列实现 BFS 广度(横向)优先遍历
+func (tree *BinaryTree) BreadthFirstTraverse(f func(int)) {
+	if tree.root == nil {
+		return
+	}
+
+	q := make([]*Node, 0)
+	q = append(q, tree.root) // root 入节点队列
+	for len(q) > 0 {
+		node := q[0]
+		q = q[1:]
+		f(node.value)
+		if node.left != nil {
+			q = append(q, node.left)
+		}
+		if node.right != nil {
+			q = append(q, node.right)
+		}
+	}
+}
+
 // 搜索
 // 有种有序数组二分查找元素的感觉
 func (tree *BinaryTree) Search(target int) (*Node) {
@@ -101,4 +124,78 @@ func (tree *BinaryTree) Search(target int) (*Node) {
 		}
 	}
 	return nil
+}
+
+// 计算树的深度
+// Node 的深度是 root 到 Node 的唯一路径长
+func (tree *BinaryTree) Depth(node *Node) int {
+	if node == nil {
+		return 0
+	}
+
+	lDepth := tree.Depth(node.left)
+	rDepth := tree.Depth(node.right)
+
+	if lDepth > rDepth { // 回溯比较左右子树的深度
+		return lDepth + 1 // 算上 node 本身
+	}
+	return rDepth + 1
+}
+
+// Node 节点数
+func (tree *BinaryTree) NodeCount(node *Node) int {
+	if node == nil {
+		return 0
+	}
+	return 1 + tree.NodeCount(node.left) + tree.NodeCount(node.right)
+}
+
+// Leaf 节点数
+func (tree *BinaryTree) LeafCount(node *Node) int {
+	if node == nil {
+		return 0
+	}
+	if node.left == nil && node.right == nil {
+		return 1
+	}
+	return tree.LeafCount(node.left) + tree.LeafCount(node.right)
+}
+
+// 原样复制
+func (tree *BinaryTree) Copy(node *Node) *Node {
+	if node == nil {
+		return nil
+	}
+	newNode := &Node{value: node.value}
+	newNode.left = tree.Copy(node.left)
+	newNode.right = tree.Copy(node.right)
+	return newNode
+}
+
+// 镜像复制
+func (tree *BinaryTree) Mirror(node *Node) *Node {
+	if node == nil {
+		return nil
+	}
+	newNode := &Node{value: node.value}
+	newNode.left = node.right
+	newNode.right = node.left
+	return newNode
+}
+
+// 判等
+// 所有子节点的位置和值均相等的两棵树
+func (tree *BinaryTree) Same(node1 *Node, node2 *Node) bool {
+	if node1 == nil && node2 == nil {
+		return true
+	}
+	if node1 == nil || node2 == nil {
+		return false
+	}
+	return node1.value == node2.value && tree.Same(node1.left, node2.left) && tree.Same(node1.right, node2.right)
+}
+
+// 删除整棵树
+func (tree *BinaryTree) Free() {
+	tree.root = nil // GC 会处理
 }
--- binary_tree/tree_test.go
+++ binary_tree/tree_test.go
@@ -35,3 +35,46 @@ func TestSearch(t *testing.T) {
 	fmt.Printf("%+v\n", tree.Search(9))  // &{value:9 left:<nil> right:0xc42000a1c0}
 	fmt.Printf("%+v\n", tree.Search(12)) // <nil>
 }
+
+func TestBreadthFirstTraverse(t *testing.T) {
+	tree.BreadthFirstTraverse(f) // 6 4 8 2 5 7 9 1 3 10
+}
+
+func TestDepth(t *testing.T) {
+	fmt.Println(tree.Depth(tree.root)) // 4
+	tree.Insert(11)
+	fmt.Println(tree.Depth(tree.root)) // 5
+}
+
+func TestCopy(t *testing.T) {
+	var newTree BinaryTree
+	newTree.root = tree.Copy(tree.root)
+	newTree.BreadthFirstTraverse(f) // 6 4 8 2 5 7 9 1 3 10
+}
+
+func TestMirror(t *testing.T) {
+	var newTree BinaryTree
+	newTree.root = tree.Mirror(tree.root)
+	newTree.BreadthFirstTraverse(f) // 6 8 4 7 9 2 5 10 1 3
+}
+
+func TestNodeCount(t *testing.T) {
+	fmt.Println(tree.NodeCount(tree.root)) // 10
+}
+
+func TestLeafCount(t *testing.T) {
+	fmt.Println(tree.LeafCount(tree.root)) // 5
+}
+
+func TestSame(t *testing.T) {
+	var newTree BinaryTree
+	newTree.root = tree.Copy(tree.root)
+	fmt.Println(tree.Same(tree.root, newTree.root)) // true
+	newTree.root.right.value = 100
+	fmt.Println(tree.Same(tree.root, newTree.root)) // false
+}
+
+func TestFree(t *testing.T) {
+	tree.Free()
+	fmt.Printf("%+v\n", tree.root) // <nil>
+}
