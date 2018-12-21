8125ab8f5f58b346f02f218f18f80a41e70d1418
wuYin <wuYinPost@gmail.com>
2018-12-21 10:11:50 +0800

    添加简单最小堆的 Push 与 Pop 操作

--- heap/heap.go
+++ heap/heap.go
@@ -1,18 +1,46 @@
 package heap
 
-type Heap []int
+type Heap []int // 堆中的元素值即优先队列中的优先度
 
-func (h *Heap) init() {
+func (h *Heap) Init() {
 	n := len(*h)
 	for i := n/2 - 1; i >= 0; i-- {
 		h.down(i, n)
 	}
 }
 
+func (h *Heap) Push(v int) {
+	*h = append(*h, v)
+	h.up(len(*h) - 1)
+}
+
+func (h *Heap) Pop() int {
+	hs := *h
+	n := len(hs) - 1
+	min := hs[0]
+	hs[0], hs[n] = hs[n], hs[0]
+	h.down(0, n)
+	*h = hs[:n]
+	return min
+}
+
+// 从位置 i 将元素上滤
+func (h *Heap) up(i int) {
+	for {
+		parent := (i - 1) / 2
+		if parent == i || h.isLess(parent, i) {
+			break
+		}
+		h.swap(parent, i) // 保证插入后依旧是最小堆
+		i = parent
+	}
+}
+
+// 将位置 mid 下滤交换
 func (h *Heap) down(mid, n int) {
 	for {
 		l := 2*mid + 1
-		if l >= n || l < 0 {
+		if l >= n || l < 0 { // l < 0 防止 int 上溢
 			break
 		}
 
@@ -21,12 +49,11 @@ func (h *Heap) down(mid, n int) {
 			min = r
 		}
 
-		if !h.isLess(min, mid) {
+		if !h.isLess(min, mid) { // 可提前退出：从 n/2-1 开始 down，能保证从最后一棵子树开始就是最小堆
 			break
 		}
 
 		h.swap(mid, min)
-
 		mid = min
 	}
 }
--- heap/heap_test.go
+++ heap/heap_test.go
@@ -6,7 +6,15 @@ import (
 )
 
 func TestHeapify(t *testing.T) {
-	h := &Heap{3, 1, 2}
-	h.init()
-	fmt.Println(h)
+	h := &Heap{8, 5, 7, 4, 1, 6, 3, 2}
+	h.Init()
+	fmt.Println(h) // [1 2 3 4 5 6 7 8] // max heap 转为了 min heap
+}
+
+func TestOperation(t *testing.T) {
+	h := &Heap{7, 5, 2, 3, 6, 4, 1}
+	h.Init()
+	fmt.Println(*h)      // [1 3 2 5 6 4 7] // min heap
+	fmt.Println(h.Pop()) // 1
+	fmt.Println(*h)      // [2 3 4 5 6 7] 	// still min heap
 }
